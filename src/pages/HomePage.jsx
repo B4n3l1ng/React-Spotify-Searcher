454bf96765f986f97c922febd84e046c4c1e33e5
Eric <eric.coimbra@gmail.com>
2023-08-10 16:30:51 +0100

    minor bug fixes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,40 @@
 import { Link } from "react-router-dom";
-import { Button, MediaQuery, rem } from "@mantine/core";
+import { Button } from "@mantine/core";
+import { SpotifyContext } from "../contexts/SpotifyContext";
+import { useContext } from "react";
 
 const HomePage = () => {
+  const { token, clientId, redirectUri, authEndpoint, responseType } =
+    useContext(SpotifyContext);
   return (
     <div className="homepage homepageBkg">
-      <div className="homeContainer">
-        <h1>Welcome to Spotify Search</h1>
-        <div className="buttonContainer">
-          <Button color="green" radius="xl" size="md">
-            <Link to="/artist-search">Search for an artist</Link>
-          </Button>
-          <Button color="green" radius="xl" size="md">
-            <Link to="/album-search">Search for an album</Link>
-          </Button>
-          <Button color="green" radius="xl" size="md">
-            <Link to="/track-search">Search for a song</Link>
+      {token ? (
+        <div className="homeContainer">
+          <h1>Welcome to Spotify Search</h1>
+          <div className="buttonContainer">
+            <Button color="green" radius="xl" size="md">
+              <Link to="/artist-search">Search for an artist</Link>
+            </Button>
+            <Button color="green" radius="xl" size="md">
+              <Link to="/album-search">Search for an album</Link>
+            </Button>
+            <Button color="green" radius="xl" size="md">
+              <Link to="/track-search">Search for a song</Link>
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <div className="homeContainer">
+          <h1>Please login to Spotify first to use our searcher app.</h1>
+          <Button color="green" gradius="xl" size="md">
+            <a
+              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}`}
+            >
+              Login to Spotify
+            </a>
           </Button>
         </div>
-      </div>
+      )}
     </div>
   );
 };
